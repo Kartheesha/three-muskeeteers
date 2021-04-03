b16807b8847b5a63de1c69a8d737e3cd906fd06d
202011065 <78265680+202011065@users.noreply.github.com>
2021-04-03 23:56:13 +0530

    Added Factorial code and executable

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int num,i,fact=1;
+
+  printf("Enter the number :");
+  scanf("%d",&num);
+
+  for(i=1;i<=num;i++)
+    {
+      fact = fact*i;
+    }
+  printf("The Factorial of %d is: %d\n",num,fact);
+  return 0;
+}
